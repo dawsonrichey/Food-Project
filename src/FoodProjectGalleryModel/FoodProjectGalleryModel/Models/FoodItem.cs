3f7800a33e86e033ecc5c86500d7f4df716a9abe
Dawson Richey <dawsonrichey@gmail.com>
2018-06-25 23:50:02 -0400

    Add Department Id to not allow null values

--- src/FoodProjectGalleryModel/FoodProjectGalleryModel/Models/FoodItem.cs
+++ src/FoodProjectGalleryModel/FoodProjectGalleryModel/Models/FoodItem.cs
@@ -10,13 +10,15 @@ namespace FoodProjectGalleryModel.Models
 	{
 		// ID, FoodItemId, FoodItemID
 		public int Id { get; set; }
+		public int DepartmentId { get; set; }
 		public string FoodName { get; set; }
 		public int QuantityOnHand { get; set; }
 		public int ItemNumber { get; set; }
 		public string Description { get; set; }
-		public Department Department { get; set; }
 		public DateTime PublishedOn { get; set; }
 		public decimal? UnitCost { get; set; }
+
+		public Department Department { get; set; }
 
 		public string DisplayText
 		{
