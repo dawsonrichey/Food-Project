ad44d210a71f94f75d8e55387f290285e7976c21
Dawson Richey <dawsonrichey@gmail.com>
2018-06-25 21:07:13 -0400

    updated the program class file use context class to add food item, retrieves a litst of food items and write the foodnames to the console

--- src/FoodProjectGalleryModel/FoodProjectGalleryModel/Program.cs
+++ src/FoodProjectGalleryModel/FoodProjectGalleryModel/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FoodProjectGalleryModel.Models;
 
 namespace FoodProjectGalleryModel
 {
@@ -10,6 +11,26 @@ namespace FoodProjectGalleryModel
 	{
 		static void Main(string[] args)
 		{
+			using (var context = new Context())
+			{
+				context.FoodItems.Add(new FoodItem()
+				{
+					FoodName = "Banana",
+					QuantityOnHand = 1,
+					PublishedOn = DateTime.Today
+				});
+				context.SaveChanges();
+
+				var foodItems = context.FoodItems.ToList();
+				foreach (var foodItem in foodItems)
+				{
+					Console.WriteLine(foodItem.FoodName);
+				}
+
+				Console.ReadLine();
+			}
+
+
 		}
 	}
 }
