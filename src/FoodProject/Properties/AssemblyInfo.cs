fb5880ca04c5f1670faf2a8c91a4c3c4cb707bf5
Dawson Richey <dawsonrichey@gmail.com>
2018-07-11 18:52:24 -0400

    Updated Assembly information  by adding a company name and project description

--- src/FoodProject/Properties/AssemblyInfo.cs
+++ src/FoodProject/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FoodProject")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("If I'm lucky this project will pass my class requirments.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Dawson Richey World Wide")]
 [assembly: AssemblyProduct("FoodProject")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
