3f0099bb0f1e6212a5447461e50373e3410a446d
Dawson Richey <dawsonrichey@gmail.com>
2018-07-22 11:15:10 -0400

    Updated Food Project Detail view

--- src/FoodProject/Controllers/FoodProjectController.cs
+++ src/FoodProject/Controllers/FoodProjectController.cs
@@ -10,6 +10,15 @@ namespace FoodProject.Controllers
 	{
 		public ActionResult Detail()
 		{
+			ViewBag.FoodName = "Carrots";
+			ViewBag.UnitCost = 7;
+			ViewBag.Description = "<p>Carrots are orange. Rabbits love to eat carrots.</p>";
+			ViewBag.Recipes = new string[]{
+				"carrot cake",
+				"carrot stew",
+				"carrot pie"
+			};
+
 			return View();
 		}
 	}
